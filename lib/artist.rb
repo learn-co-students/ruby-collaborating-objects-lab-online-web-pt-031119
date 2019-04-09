64ba5736f2df568c6421c71729609846e6308a50
Kat B <katreesebarnes@gmail.com>
2019-04-09 20:29:50 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,17 @@
+class Artist 
+  attr_accessor :name, :songs
+  # @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  # def self.all 
+  #   @@all
+  # end
+  def add_song(song)
+    @songs << song
+  end
+ 
+
+end
