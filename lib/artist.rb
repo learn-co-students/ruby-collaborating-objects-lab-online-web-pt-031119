0f85ca9fb28f4403b72b24506b50845469c57af2
Ryan <kerneltux0@gmail.com>
2019-04-11 09:37:45 -0600

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,37 +1,38 @@
 require 'pry'
 
 class Artist
-  attr_accessor :name, :songs
-  @@all = []
+	attr_accessor :name, :songs
+	@@all = []
+	
+	def initialize(name)
+		@name = name
+		@songs = []
+	end
 
-  def initialize(name)
-    @name = name
-    @songs = []
+	def self.all
+		@@all
+	end
 
-  end
+	def add_song(song)
+		self.songs << song
+	end
 
-  def add_song(song)
-    @songs << song
+	def save
+		@@all << self
+	end
 
-  end
+	def self.find_or_create_by_name(name)
+		#binding.pry
+		if @@all.find {|artist| artist.name == name}
+			@@all.find {|artist| artist.name == name}
+		else
+			new_artist = self.new(name)
+			new_artist.save
+			new_artist
+		end
+	end
 
-  def save
-    @@all << self
-
-  end
-
-  def self.all
-    @@all
-  end
-
-  def self.find_or_create_by_name(name)
-    #binding.pry
-    #new_artist = self.new(name) if @@all.include?(self.name) == false
-    #new_artist.save
-    #return new_artist
-    @@all.include?(name) ? @@all.find(name) : self.new(name).tap {|artist| artist.save}
-    #binding.pry
-  end
-
-  
+	def print_songs
+		songs.each {|song| puts song.name}
+	end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,28 @@
+require 'pry'
+
 class Song
+	attr_accessor :name
+	attr_reader :artist
 
+	def initialize(name)
+		@name = name
+	end
 
-  def initialize(name)
+	def artist=(artist)
+		@artist = artist
+	end
 
-  end
+	def artist_name=(name)
+		self.artist = Artist.find_or_create_by_name(name)
+		artist.add_song(self)
+	end
+
+	def self.new_by_filename(file)
+		artist, song = file.split(' - ')
+		new_song = self.new(song)
+		new_song.artist_name = artist
+		new_song
+		#binding.pry
+	end
 
 end
